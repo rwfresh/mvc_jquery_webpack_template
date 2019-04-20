9f87ab7e51f251104bac6c28d66f2984a66ccab7
rich williams <rich@wifidelity.com>
2019-04-20 11:59:32 -0400

    .gitignore fix

--- wwwroot/dist/0.js
+++
@@ -1,5 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"./src/newJsPage.js":
-/*!**************************!*\
-  !*** ./src/newJsPage.js ***!
-  \**************************/
-/*! exports provided: newJsPage */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "newJsPage", function() { return newJsPage; });\nvar newJsPage = function newJsPage(testVariableFromController) {\n  $(\'#messageOnThisPage\').text(testVariableFromController);\n  $(".dropdown-toggle").dropdown();\n  console.log("hey!");\n};\n\n//# sourceURL=webpack:///./src/newJsPage.js?')}}]);
